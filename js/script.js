192a126a60ad018cf8dab5e23848d2e4154f2bba
Yulii Shtel <ulik1shtel@gmail.com>
2022-12-06 17:24:58 +0200

    kind of sticky navigation

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const sectionHero = document.querySelector('.section-hero');
+const sectionAbout = document.querySelector('.about-section');
+const sectionSkills = document.querySelector('.skills-section');
+const sectionContact = document.querySelector('.contact-section');
+const nav = document.querySelector('.header-nav');
+
+/***************************/
+/* Navigation */
+/***************************/
+
+// Sticky navigation
+const header = document.querySelector('.header');
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  treshold: 0,
+  rootMargin: `-40px`,
+});
+
+headerObserver.observe(header);
