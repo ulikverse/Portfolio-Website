a1ffc7e16730af2185b5c5de12aace9b6b1d2081
Yulii Shtel <ulik1shtel@gmail.com>
2022-12-08 17:40:19 +0200

    smooth scroll updated

--- js/script.js
+++ js/script.js
@@ -14,6 +14,33 @@ const nav = document.querySelector('.header-nav');
 /* Navigation */
 /***************************/
 
+// Smooth scroll
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
 // Sticky navigation
 // const header = document.querySelector('.header');
 
