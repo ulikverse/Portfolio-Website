ed1ded1f68c6d856e8d0360159e0d6865bad90ec
Yulii Shtel <ulik1shtel@gmail.com>
2022-12-12 15:46:29 +0200

    FINALLY FIXED CLICKING BUG

--- js/script.js
+++ js/script.js
@@ -15,31 +15,31 @@ const nav = document.querySelector('.header-nav');
 /***************************/
 
 // Smooth scroll
-const allLinks = document.querySelectorAll('a:link');
+// const allLinks = document.querySelectorAll('a:link');
 
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
+// allLinks.forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
 
-    // Scroll back to top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
+//     // Scroll back to top
+//     if (href === '#')
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//       });
 
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
-    }
+//     // Scroll to other links
+//     if (href !== '#' && href.startsWith('#')) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: 'smooth' });
+//     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  });
-});
+//     // Close mobile naviagtion
+//     if (link.classList.contains('main-nav-link'))
+//       headerEl.classList.toggle('nav-open');
+//   });
+// });
 
 // Sticky navigation
 // const header = document.querySelector('.header');
