6d083b7ab0a4f8464b2f180b7525accbe839227c
Yulii Shtel <ulik1shtel@gmail.com>
2022-12-13 23:20:54 +0200

    reveal sections

--- js/script.js
+++ js/script.js
@@ -4,90 +4,20 @@ if (module.hot) {
   module.hot.accept();
 }
 
-const sectionHero = document.querySelector('.section-hero');
-const sectionAbout = document.querySelector('.about-section');
-const sectionSkills = document.querySelector('.skills-section');
-const sectionContact = document.querySelector('.contact-section');
-const nav = document.querySelector('.header-nav');
+function reveal() {
+  const reveals = document.querySelectorAll('.reveal');
 
-/***************************/
-/* Navigation */
-/***************************/
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
 
-// Smooth scroll
-// const allLinks = document.querySelectorAll('a:link');
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
 
-// allLinks.forEach(function (link) {
-//   link.addEventListener('click', function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute('href');
-
-//     // Scroll back to top
-//     if (href === '#')
-//       window.scrollTo({
-//         top: 0,
-//         behavior: 'smooth',
-//       });
-
-//     // Scroll to other links
-//     if (href !== '#' && href.startsWith('#')) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: 'smooth' });
-//     }
-
-//     // Close mobile naviagtion
-//     if (link.classList.contains('main-nav-link'))
-//       headerEl.classList.toggle('nav-open');
-//   });
-// });
-
-// Sticky navigation
-// const header = document.querySelector('.header');
-
-// const stickyNav = function (entries) {
-//   const [entry] = entries;
-//   // console.log(entry);
-
-//   if (!entry.isIntersecting) nav.classList.add('sticky');
-//   else nav.classList.remove('sticky');
-// };
-
-// const headerObserver = new IntersectionObserver(stickyNav, {
-//   root: null,
-//   treshold: 0.1,
-//   // rootMargin: `-20px`,
-// });
-
-// headerObserver.observe(header);
-
-/***************************/
-/* Reveal sections */
-/***************************/
-
-// const allSections = document.querySelectorAll('.section');
-
-// const revealSection = function (entries, observer) {
-//   const [entry] = entries;
-
-//   if (!entry.isIntersecting) return;
-//   entry.target.classList.remove('section--hidden');
-//   observer.unobserve(entry.target);
-// };
-
-// const sectionObserver = new IntersectionObserver(revealSection, {
-//   root: null,
-//   threshold: 0.15,
-// });
-// allSections.forEach(function (section) {
-//   sectionObserver.observe(section);
-//   section.classList.add('section--hidden');
-// });
-
-/*
-// Testing code
-window.onload = function () {
-  document.getElementById('btn1').onclick = function () {
-    console.log('button was clicked');
-  };
-};
-*/
+window.addEventListener('scroll', reveal);
